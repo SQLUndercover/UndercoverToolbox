9ea99065e1bbcc782e63afeef92e378dac70793b
SQLUndercover <david.fowler1@outlook.com>
2018-01-05 10:08:23 +0000

    sp_AGReconfigure

--- sp_AGReconfigure.sql
+++ sp_AGReconfigure.sql
@@ -7,14 +7,30 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-/**********************************************
---Procedure Name: sp_AGreconfigure
---Author: Adrian Buckman
+/******************************************************************
 
---Description: Produce statements to ALTER Synchronisation mode and Auto Failover Mode
---www.SQLUndercover.com
+Author: Adrian Buckman
+Revision date: 08/12/2017
+Version: V1
 
-*********************************************/
+© www.sqlundercover.com 
+
+
+This script is for personal, educational, and internal 
+corporate purposes, provided that this header is preserved. Redistribution or sale 
+of this script,in whole or in part, is prohibited without the author's express 
+written consent. 
+
+The software is provided "as is", without warranty of any kind, express or
+implied, including but not limited to the warranties of merchantability,
+fitness for a particular purpose and noninfringement. in no event shall the
+authors or copyright holders be liable for any claim, damages or other
+liability, whether in an action of contract, tort or otherwise, arising from,
+out of or in connection with the software or the use or other dealings in the
+software.
+
+******************************************************************/
+
 
 CREATE PROCEDURE [dbo].[sp_AGreconfigure]
 (
