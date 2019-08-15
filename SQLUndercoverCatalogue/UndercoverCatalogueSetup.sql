aaae7f11e98a34306322073a1bb1b27d45916da5
UndercoverDavid <david@sqlundercover.com>
2019-08-15 12:26:09 +0100

    Catalogue 0.3 Release Candidate 2

--- SQLUndercoverCatalogue/UndercoverCatalogueSetup.sql
+++ SQLUndercoverCatalogue/UndercoverCatalogueSetup.sql
@@ -3363,8 +3363,6 @@ BEGIN
 											deleted.[Collation], 
 											deleted.[Edition], 
 											deleted.[VersionNo], 
-											deleted.[FirstRecorded], 
-											deleted.[LastRecorded], 
 											deleted.[CustomerName], 
 											deleted.[ApplicationName], 
 											deleted.[Notes], 
@@ -3495,6 +3493,49 @@ GO
 ALTER TABLE [Catalogue].[Users] ENABLE TRIGGER [AuditUsers]
 GO
 
+
+--modification to UpdateLogins, roles weren't being collected properly before
+
+ALTER PROC [Catalogue].[UpdateLogins]
+AS
+BEGIN
+
+--update logins where they are known
+UPDATE	Catalogue.Logins 
+SET		ServerName = [Logins_Stage].ServerName,
+		LoginName = [Logins_Stage].LoginName,
+		SID = [Logins_Stage].SID,
+		RoleName = [Logins_Stage].RoleName,
+		PasswordHash = [Logins_Stage].PasswordHash,
+		LastRecorded = GETDATE(),
+		IsDisabled = [Logins_Stage].IsDisabled,
+		LoginType = [Logins_Stage].LoginType
+FROM	[Catalogue].[Logins_Stage]
+WHERE	Logins.ServerName = [Logins_Stage].ServerName
+		AND Logins.LoginName = [Logins_Stage].LoginName
+		AND Logins.RoleName = [Logins_Stage].RoleName
+
+--insert logins that are unknown to the catlogue
+INSERT INTO Catalogue.Logins
+(ServerName,LoginName,SID,RoleName,FirstRecorded,LastRecorded, IsDisabled, PasswordHash,LoginType)
+SELECT ServerName,
+		LoginName,
+		SID,
+		RoleName,
+		GETDATE(),
+		GETDATE(),
+		IsDisabled,
+		PasswordHash,
+		LoginType
+FROM [Catalogue].[Logins_Stage]
+WHERE NOT EXISTS 
+(SELECT 1 FROM Catalogue.Logins
+		WHERE Logins.ServerName = [Logins_Stage].ServerName
+		AND Logins.LoginName = [Logins_Stage].LoginName
+		AND Logins.RoleName = [Logins_Stage].RoleName)
+
+END
+GO
 
 --update version number
 UPDATE Catalogue.ConfigPoSH
