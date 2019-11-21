6a13b8b7ef697b0da725b756d7c789e3a0db5b8f
UndercoverDavid <david@sqlundercover.com>
2019-11-21 16:20:51 +0000

    manifest update

--- SQLUndercoverCatalogue/Updates/Catalogue_UD040.sql
+++ SQLUndercoverCatalogue/Updates/Catalogue_UD040.sql
@@ -187,3 +187,50 @@ FROM #FinalResults
 END'
 
 where ModuleID = 8
+
+
+--fix issue with Tables module
+
+
+UPDATE Catalogue.ConfigModulesDefinitions
+SET UpdateDefinition = 
+'BEGIN
+
+--update tables where they are known to the catalogue
+UPDATE Catalogue.Tables 
+SET		ServerName = Tables_Stage.ServerName
+		,DatabaseName = Tables_Stage.DatabaseName
+		,SchemaName = Tables_Stage.SchemaName
+		,TableName = Tables_Stage.TableName
+		,Columns = Tables_Stage.Columns
+		,LastRecorded = GETDATE()
+FROM	Catalogue.Tables_Stage
+WHERE	Tables.ServerName = Tables_Stage.ServerName
+		AND Tables.SchemaName = Tables_Stage.SchemaName
+		AND Tables.TableName = Tables_Stage.TableName
+		AND Tables.DatabaseName = Tables_Stage.DatabaseName
+
+
+
+--insert tables that are unknown to the catlogue
+INSERT INTO Catalogue.Tables
+(ServerName,DatabaseName,SchemaName,TableName,Columns,FirstRecorded,LastRecorded)
+SELECT ServerName,
+		DatabaseName,
+		SchemaName,
+		TableName,
+		Columns,
+		GETDATE(),
+		GETDATE()
+FROM Catalogue.Tables_Stage
+WHERE NOT EXISTS 
+(SELECT 1 FROM Catalogue.Tables
+WHERE	Tables.ServerName = Tables_Stage.ServerName
+		AND Tables.SchemaName = Tables_Stage.SchemaName
+		AND Tables.TableName = Tables_Stage.TableName
+		AND Tables.DatabaseName = Tables_Stage.DatabaseName)
+
+END'
+
+WHERE ModuleID = 10
+
