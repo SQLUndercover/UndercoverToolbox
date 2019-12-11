1587125115e1562f5f998e79bd713ff4a7a1a694
UndercoverDavid <david@sqlundercover.com>
2019-12-11 15:57:34 +0000

    Enhanced Services Module

---
+++ SQLUndercoverCatalogue/ModuleDefinitions/GetServicesEnhanced.sql
@@ -0,0 +1,138 @@
+--Undercover Catalogue
+--David Fowler
+--Version 0.4.0 - 11 December 2019
+--Module: ServicesEnhanced
+--Script: Get
+
+IF (OBJECT_ID('tempdb.dbo.#RawServices') IS NOT NULL)
+DROP TABLE #RawServices
+
+IF (OBJECT_ID('tempdb.dbo.#Services') IS NOT NULL)
+DROP TABLE #Services
+
+DECLARE @ServiceName VARCHAR(256)
+DECLARE @cmd NVARCHAR(500)
+
+CREATE TABLE #RawServices
+(	ServiceName VARCHAR(256) NULL,
+	RowNo INT IDENTITY(1,1))
+
+CREATE TABLE #Services
+(
+    ServerName SYSNAME NULL,
+    ServiceName NVARCHAR(256) NULL,
+    StartupType NVARCHAR(256) NULL,
+    StatusDesc NVARCHAR(256) NULL,
+    ServiceAccount NVARCHAR(256) NULL,
+    InstantFileInit INT NULL
+)
+
+
+--populate #RawServices with SQL related services
+
+INSERT INTO #RawServices(ServiceName)
+EXEC xp_cmdshell 'sc query type= service state= all'-- |find "MSOLAP"'-- |find /V "DISPLAY_NAME"'
+
+
+--sanitise data
+DELETE FROM #RawServices 
+WHERE	ServiceName IS NULL
+	OR	(ServiceName NOT LIKE 'SERVICE_NAME:%' AND ServiceName NOT LIKE '%STATE%:%')
+
+UPDATE #RawServices
+SET ServiceName = CASE	WHEN CHARINDEX('RUNNING', ServiceName) > 0 THEN 'Running'
+						WHEN CHARINDEX('STOPPED', ServiceName) > 0 THEN 'Stopped'
+						ELSE 'Other' 
+				END
+WHERE ServiceName NOT LIKE 'SERVICE_NAME:%'
+
+UPDATE #RawServices
+SET ServiceName = REPLACE(ServiceName, 'SERVICE_NAME: ','')
+
+--Get running state
+INSERT INTO #Services(ServerName,ServiceName,StatusDesc)
+SELECT @@SERVERNAME, ServiceName, State
+FROM
+	(SELECT ServiceName, ROW_NUMBER() OVER (ORDER BY rowno) AS ServiceID 
+	FROM #RawServices
+	WHERE ServiceName NOT IN ('RUNNING','STOPPED','START_PENDING','STOP_PENDING','UNKNOWN')) AS ServicesNames
+JOIN
+	(SELECT ServiceName AS State, ROW_NUMBER() OVER (ORDER BY rowno) AS ServiceID 
+	FROM #RawServices
+	WHERE ServiceName IN ('RUNNING','STOPPED','START_PENDING','STOP_PENDING','UNKNOWN')) AS States ON States.ServiceID = ServicesNames.ServiceID
+
+--remove the services that we're no worried about
+
+DELETE FROM #Services
+WHERE   ServiceName NOT LIKE 'MSOLAP%'
+    AND  ServiceName NOT LIKE 'MsDtsServer%'
+    AND  ServiceName != 'SQLServerReportingServices' 
+    AND  ServiceName NOT LIKE 'ReportServer%'
+    AND  ServiceName != 'SQL Server Distributed Replay Client'
+    AND  ServiceName != 'SQL Server Distributed Replay Controller'
+
+
+
+DECLARE ServicesCur CURSOR STATIC FORWARD_ONLY FOR
+SELECT ServiceName
+FROM #Services
+
+--EXEC xp_cmdshell 'sc qc BrokerInfrastructure'
+
+OPEN ServicesCur
+
+--fetch service details
+FETCH NEXT FROM ServicesCur INTO @ServiceName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+    TRUNCATE TABLE #RawServices
+
+	SET @cmd = 'sc qc "' + @ServiceName + '"'
+
+    INSERT #RawServices(ServiceName)
+	EXEC xp_cmdshell @cmd
+
+    --Update with Startup Type
+    UPDATE #Services
+    SET StartupType = CASE  WHEN CHARINDEX('DISABLED', RawServices.ServiceName) > 0 THEN 'Disabled'
+                            WHEN CHARINDEX('AUTO_START', RawServices.ServiceName) > 0 THEN 'Automatic'
+                            WHEN CHARINDEX('DEMAND_START', RawServices.ServiceName) > 0 THEN 'Manual'
+                            ELSE 'Other'
+                        END
+    FROM #RawServices RawServices
+    WHERE RawServices.ServiceName LIKE '%START_TYPE%'
+    AND #Services.ServiceName = @ServiceName
+
+    --Update with Service Account
+    UPDATE #Services
+    SET ServiceAccount = REPLACE(RawServices.ServiceName,'        SERVICE_START_NAME : ', '')
+    FROM #RawServices RawServices
+    WHERE RawServices.ServiceName LIKE '%SERVICE_START_NAME%'
+    AND #Services.ServiceName = @ServiceName
+
+
+    --Update with service display name
+    UPDATE #Services
+    SET ServiceName = REPLACE(RawServices.ServiceName, '        DISPLAY_NAME       : ','')
+    FROM #RawServices RawServices
+    WHERE RawServices.ServiceName LIKE '%DISPLAY_NAME%:%'
+    AND #Services.ServiceName = @ServiceName
+
+
+FETCH NEXT FROM ServicesCur INTO @ServiceName
+
+END
+
+CLOSE ServicesCur
+DEALLOCATE ServicesCur
+
+SELECT  ServerName,
+        ServiceName,
+        StartupType,
+        StatusDesc,
+        ServiceAccount,
+        InstantFileInit 
+FROM #Services
+
--- SQLUndercoverCatalogue/Updates/Catalogue_UD040.sql
+++ SQLUndercoverCatalogue/Updates/Catalogue_UD040.sql
@@ -1674,3 +1674,197 @@ BEGIN
 END
 GO
 
+--enhanced services module - this on piggy backs on the services module's tables.  
+--The code's a little dirty and it breaks out into xp_cmdshell so starts off disabled by default
+
+INSERT INTO Catalogue.ConfigModules (ModuleName,GetProcName,UpdateProcName,StageTableName,MainTableName,Active)
+VALUES ('ServicesEnhanced','GetServicesEnhanced','UpdateServicesEnhanced','Services_Stage','Services',0)
+GO
+
+
+DECLARE @ModuleID INT
+
+SELECT @ModuleID = ID
+FROM	Catalogue.ConfigModules
+WHERE ConfigModules.ModuleName = 'ServicesEnhanced'
+
+
+INSERT INTO Catalogue.ConfigModulesDefinitions (ModuleID,Online,GetDefinition,UpdateDefinition,GetURL,UpdateURL)
+VALUES	(@ModuleID,
+		1,'--Undercover Catalogue
+--David Fowler
+--Version 0.4.0 - 11 December 2019
+--Module: ServicesEnhanced
+--Script: Get
+
+IF (OBJECT_ID(''tempdb.dbo.#RawServices'') IS NOT NULL)
+DROP TABLE #RawServices
+
+IF (OBJECT_ID(''tempdb.dbo.#Services'') IS NOT NULL)
+DROP TABLE #Services
+
+DECLARE @ServiceName VARCHAR(256)
+DECLARE @cmd NVARCHAR(500)
+
+CREATE TABLE #RawServices
+(	ServiceName VARCHAR(256) NULL,
+	RowNo INT IDENTITY(1,1))
+
+CREATE TABLE #Services
+(
+    ServerName SYSNAME NULL,
+    ServiceName NVARCHAR(256) NULL,
+    StartupType NVARCHAR(256) NULL,
+    StatusDesc NVARCHAR(256) NULL,
+    ServiceAccount NVARCHAR(256) NULL,
+    InstantFileInit INT NULL
+)
+
+
+--populate #RawServices with SQL related services
+
+INSERT INTO #RawServices(ServiceName)
+EXEC xp_cmdshell ''sc query type= service state= all''
+
+
+--sanitise data
+DELETE FROM #RawServices 
+WHERE	ServiceName IS NULL
+	OR	(ServiceName NOT LIKE ''SERVICE_NAME:%'' AND ServiceName NOT LIKE ''%STATE%:%'')
+
+UPDATE #RawServices
+SET ServiceName = CASE	WHEN CHARINDEX(''RUNNING'', ServiceName) > 0 THEN ''Running''
+						WHEN CHARINDEX(''STOPPED'', ServiceName) > 0 THEN ''Stopped''
+						ELSE ''Other'' 
+				END
+WHERE ServiceName NOT LIKE ''SERVICE_NAME:%''
+
+UPDATE #RawServices
+SET ServiceName = REPLACE(ServiceName, ''SERVICE_NAME: '','''')
+
+--Get running state
+INSERT INTO #Services(ServerName,ServiceName,StatusDesc)
+SELECT @@SERVERNAME, ServiceName, State
+FROM
+	(SELECT ServiceName, ROW_NUMBER() OVER (ORDER BY rowno) AS ServiceID 
+	FROM #RawServices
+	WHERE ServiceName NOT IN (''RUNNING'',''STOPPED'',''START_PENDING'',''STOP_PENDING'',''UNKNOWN'')) AS ServicesNames
+JOIN
+	(SELECT ServiceName AS State, ROW_NUMBER() OVER (ORDER BY rowno) AS ServiceID 
+	FROM #RawServices
+	WHERE ServiceName IN (''RUNNING'',''STOPPED'',''START_PENDING'',''STOP_PENDING'',''UNKNOWN'')) AS States ON States.ServiceID = ServicesNames.ServiceID
+
+--remove the services that we''re no worried about
+
+DELETE FROM #Services
+WHERE   ServiceName NOT LIKE ''MSOLAP%''
+    AND  ServiceName NOT LIKE ''MsDtsServer%''
+    AND  ServiceName != ''SQLServerReportingServices''
+    AND  ServiceName NOT LIKE ''ReportServer%''
+    AND  ServiceName != ''SQL Server Distributed Replay Client''
+    AND  ServiceName != ''SQL Server Distributed Replay Controller''
+
+
+
+DECLARE ServicesCur CURSOR STATIC FORWARD_ONLY FOR
+SELECT ServiceName
+FROM #Services
+
+OPEN ServicesCur
+
+--fetch service details
+FETCH NEXT FROM ServicesCur INTO @ServiceName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+    TRUNCATE TABLE #RawServices
+
+	SET @cmd = ''sc qc "'' + @ServiceName + ''"''
+
+    INSERT #RawServices(ServiceName)
+	EXEC xp_cmdshell @cmd
+
+    --Update with Startup Type
+    UPDATE #Services
+    SET StartupType = CASE  WHEN CHARINDEX(''DISABLED'', RawServices.ServiceName) > 0 THEN ''Disabled''
+                            WHEN CHARINDEX(''AUTO_START'', RawServices.ServiceName) > 0 THEN ''Automatic''
+                            WHEN CHARINDEX(''DEMAND_START'', RawServices.ServiceName) > 0 THEN ''Manual''
+                            ELSE ''Other''
+                        END
+    FROM #RawServices RawServices
+    WHERE RawServices.ServiceName LIKE ''%START_TYPE%''
+    AND #Services.ServiceName = @ServiceName
+
+    --Update with Service Account
+    UPDATE #Services
+    SET ServiceAccount = REPLACE(RawServices.ServiceName,''        SERVICE_START_NAME : '', '''')
+    FROM #RawServices RawServices
+    WHERE RawServices.ServiceName LIKE ''%SERVICE_START_NAME%''
+    AND #Services.ServiceName = @ServiceName
+
+
+    --Update with service display name
+    UPDATE #Services
+    SET ServiceName = REPLACE(RawServices.ServiceName, ''        DISPLAY_NAME       : '','''')
+    FROM #RawServices RawServices
+    WHERE RawServices.ServiceName LIKE ''%DISPLAY_NAME%:%''
+    AND #Services.ServiceName = @ServiceName
+
+
+FETCH NEXT FROM ServicesCur INTO @ServiceName
+
+END
+
+CLOSE ServicesCur
+DEALLOCATE ServicesCur
+
+SELECT  ServerName,
+        ServiceName,
+        StartupType,
+        StatusDesc,
+        ServiceAccount,
+        InstantFileInit 
+FROM #Services
+
+',
+		'--Undercover Catalogue
+		--David Fowler
+		--Version 0.4.0 - 10 December 2019
+		--Module: Services
+		--Script: Update
+
+		--update where known to catalogue
+		UPDATE Catalogue.Services
+		SET		ServerName = Services_Stage.ServerName,
+				ServiceName = Services_Stage.ServiceName,
+				StartupType = Services_Stage.StartupType,
+				StatusDesc = Services_Stage.StatusDesc,
+				ServiceAccount = Services_Stage.ServiceAccount,
+				InstantFileInit = Services_Stage.InstantFileInit,
+				LastRecorded = GETDATE()
+		FROM	Catalogue.Services_Stage
+		WHERE	Services.ServerName = Services_Stage.ServerName
+		AND		Services.ServiceName = Services_Stage.ServiceName
+
+		--insert where not known to catalogue
+		INSERT INTO Catalogue.Services 
+		(ServerName, ServiceName, StartupType,StatusDesc, ServiceAccount, InstantFileInit, FirstRecorded, LastRecorded)
+		SELECT	ServerName,
+				ServiceName,
+				StartupType,
+				StatusDesc,
+				ServiceAccount, 
+				InstantFileInit, 
+				GETDATE(),
+				GETDATE()
+		FROM	Catalogue.Services_Stage
+		WHERE NOT EXISTS 
+		(SELECT 1 FROM Catalogue.Services
+		WHERE	Services.ServerName = Services_Stage.ServerName
+		AND		Services.ServiceName = Services_Stage.ServiceName)',
+		'https://raw.githubusercontent.com/SQLUndercover/UndercoverToolbox/Catalogue-v0.4-dev/SQLUndercoverCatalogue/ModuleDefinitions/GetServicesEnhanced.sql',
+		'https://raw.githubusercontent.com/SQLUndercover/UndercoverToolbox/Catalogue-v0.4-dev/SQLUndercoverCatalogue/ModuleDefinitions/UpdateServicesEnhanced.sql')
+
+GO
+
