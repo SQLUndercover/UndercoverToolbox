7b134328a2c83270e6112400f99947d56f3f8e55
Adrian Buckman <33764798+Adedba@users.noreply.github.com>
2020-01-21 00:08:34 +0000

    Powershell AutoUpdate function
    #202 - New function used in Invoke-SQLUndercoverInspector

---
+++ SQLUndercoverInspector/V2 - Additional files/InspectorAutoUpdate.psm1
@@ -0,0 +1,247 @@
+﻿function InspectorAutoUpdate {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, Position = 0, ValueFromPipelineByPropertyName)]
+        [Alias('SqlInstance', 'ServerName', 'Server')]
+        [String]$CentralServer,
+
+        [Parameter(Mandatory, Position = 1, ValueFromPipelineByPropertyName)]
+        [Alias('Database', 'DatabaseName')]
+        [String]$LoggingDb,
+
+        [Parameter(Position = 2, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('ScriptPath')]
+        [String]$Scriptfilepath = 'C:\Temp\'
+        )
+
+        [double]$RequiredInspectorBuild = 2.1
+
+
+        
+        $Serverlist = @();
+  
+        #region Set SQL Statement text 
+
+        #Set config query
+        $ConfigQuery = "
+        SELECT 
+        [Description],
+        CASE
+            WHEN [Description] = 'PSAutoUpdateModules' AND [Value] IS NULL THEN '1'
+            WHEN [Description] = 'PSAutoUpdateModulesFrequencyMins' AND [Value] IS NULL THEN '1440'
+        ELSE [Value]
+        END AS [Value]
+        FROM [Inspector].[Settings] 
+        WHERE [Description] IN ('InspectorBuild','PSAutoUpdateModules','PSAutoUpdateModulesFrequencyMins')
+        UNION ALL
+        SELECT 
+        [Updatename],
+        ISNULL(CONVERT(VARCHAR(20),[LastUpdated],113),'01/01/1900 00:00:00')
+        FROM [Inspector].[PSAutoUpdate]
+        WHERE [Updatename] = 'PSAutoUpdate';"
+
+        #Set Installed build query
+        $InstalledVersioncmd = "SELECT 
+        CASE 
+        	WHEN [SetupCommand] LIKE 'EXEC \[Inspector\].\[InspectorSetup\]%' ESCAPE '\' THEN 'InspectorSetup'
+        	ELSE [SetupCommand]
+        END AS Modulename,
+        ISNULL(CONVERT(VARCHAR(20),MAX([RevisionDate]),113),'01/01/1900 00:00:00') AS RevisionDate
+        FROM [Inspector].[InspectorUpgradeHistory] 
+        GROUP BY 
+        CASE 
+        	WHEN [SetupCommand] LIKE 'EXEC \[Inspector\].\[InspectorSetup\]%' ESCAPE '\' THEN 'InspectorSetup'
+        	ELSE [SetupCommand]
+        END;"
+
+        #Inspector setup stored procedure query.
+        $ExecInspectorSetupProc = "EXEC [Inspector].[InspectorSetup]
+        --Required Parameters						     
+        @Databasename = '$LoggingDb',	
+        @DataDrive = 'S,U',	
+        @LogDrive = 'T,V',	
+        --Optional Parameters (Defaults Specified), ignored when @InitialSetup = 0
+        @BackupsPath = 'F:\Backups',
+        @LinkedServername = NULL,  
+        @StackNameForEmailSubject = 'SQLUndercover',	
+        @EmailRecipientList = NULL,	  
+        @DriveSpaceHistoryRetentionInDays = 90, 
+        @DaysUntilDriveFullThreshold = 56, 
+        @FreeSpaceRemainingPercent = 10,
+        @DriveLetterExcludes = NULL, 
+        @DatabaseGrowthsAllowedPerDay = 1,  
+        @MAXDatabaseGrowthsAllowedPerDay = 10, 
+        @AgentJobOwnerExclusions = 'sa', 
+        @FullBackupThreshold = 8,		
+        @DiffBackupThreshold = 2,		
+        @LogBackupThreshold = 20,		
+        @DatabaseOwnerExclusions = 'sa',  
+        @LongRunningTransactionThreshold = 300,	
+        @StartTime = '08:55',
+        @EndTime = '17:30',
+        @EnableAgentJob = 1,
+        @InitialSetup = 0;"
+
+        #endregion
+
+        #Get Active server list
+        $Serverlist = Invoke-sqlcmd -ServerInstance $CentralServer -Database $LoggingDb -Query "SELECT [Servername] FROM [Inspector].[CurrentServers] WHERE [IsActive] = 1 ORDER BY CASE WHEN [Servername] = @@SERVERNAME THEN 1 ELSE 2 END ASC;"
+
+        #Get config from the central server
+        $Config = Invoke-sqlcmd -ServerInstance $CentralServer -Database $LoggingDb -Query $ConfigQuery;
+                    
+        #Assign values to config variables.
+        $CentralInspectorVersion = ($Config | ?{$_.Description -eq "InspectorBuild"}).Value
+        $AutoUpdate = ($Config | ?{$_.Description -eq "PSAutoUpdateModules"}).Value
+        [int]$PSAutoUpdateModulesFrequencyMins = ($Config | ?{$_.Description -eq "PSAutoUpdateModulesFrequencyMins"}).Value
+        [datetime]$PSAutoUpdateLastUpdated = ($Config | ?{$_.Description -eq "PSAutoUpdate"}).Value           
+
+
+        #If auto update is enabled and the last updated datetime is NULL or less than the current time run through the update check and apply updates
+        if (($AutoUpdate -eq 1) -and ((get-date -Format yyyyMMddHHmmss)) -ge (get-date $($PSAutoUpdateLastUpdated.AddMinutes($PSAutoUpdateModulesFrequencyMins)) -Format yyyyMMddHHmmss)) {
+
+            #region check Scriptfilelocation exists and create if not
+            IF ($Scriptfilepath.EndsWith("\") -eq $false) {
+                $Scriptfilepath = $Scriptfilepath+"\";
+            }
+            
+            IF ($(test-path -Path $Scriptfilepath) -eq $false) {
+                New-Item -ItemType Directory -Path $Scriptfilepath | out-null;
+            }
+            #endregion
+
+            Write-Host "Auto Update: Enabled" -ForegroundColor Yellow;
+
+            IF((get-date $PSAutoUpdateLastUpdated -format yyyyMMdd) -eq "19000101"){
+                write-host "Last Updated: Never"-ForegroundColor Yellow;
+            } ELSE {
+                write-host "Update Frequency: Every $($PSAutoUpdateModulesFrequencyMins) mins" -ForegroundColor Yellow;
+                write-host "Last Updated: $PSAutoUpdateLastUpdated" -ForegroundColor Yellow;
+                write-host "Next Update: $(get-date $($PSAutoUpdateLastUpdated.AddMinutes($PSAutoUpdateModulesFrequencyMins)) -Format "dd/MM/yyyy HH:mm:ss")" -ForegroundColor Yellow;
+            }
+
+            Write-Host "Checking for updates..." -ForegroundColor Yellow;
+            write-host "" -ForegroundColor Yellow
+ 
+            #Download Manifest
+            $ManifestPath = $($Scriptfilepath)+"Manifest.csv";
+            Try {
+                Invoke-WebRequest "https://raw.githubusercontent.com/SQLUndercover/UndercoverToolbox/Inspector-Dev/SQLUndercoverInspector/V2%20-%20Additional%20files/Manifest.csv" -OutFile $ManifestPath;       
+            } Catch{
+                write-host "Error retrieving the manifest file" -ForegroundColor Red;
+                write-host "$_.Exception.Message" -ForegroundColor Red;
+                remove-item -LiteralPath $ManifestPath;
+                Return;
+            }
+
+            $Manifest = import-csv -Path $ManifestPath
+
+            #For each server check the current build an update accordingly
+            $Serverlist | ForEach-Object{
+                write-host "Checking version information for server $($_.Servername)" -ForegroundColor Yellow;
+
+
+                #region Check local Inspector build meets the minimum requirement
+                $InspectorVersion = Invoke-sqlcmd -ServerInstance $($_.Servername) -Database $LoggingDb -Query "EXEC [Inspector].[PSGetInspectorBuild];"
+                
+                #if the local version is less than $RequiredInspectorBuild then set the MinVersionUpdateRequired flag to install the latest Inspector version
+                IF($InspectorVersion.Build -lt $RequiredInspectorBuild) {
+                    write-host "    Local server Inspector build does not meet the minumum required version ($MinimumVersion), Manual update required" -ForegroundColor Red
+                    Continue;
+                }
+            
+            
+                    #check for updates
+                    foreach($Manifestitem in $Manifest) {
+                        $Modulename = $Manifestitem.Modulename
+                        $LastUpdated = $Manifestitem.LastUpdated
+                        $URL = $Manifestitem.URL
+                        $Servername = $_.Servername
+                        $UpdateMessage = "";
+                        $InstalledVersion = @();
+                        $UpdateStmt = "";
+                        $Scriptfile = ($Scriptfilepath+$Modulename.Replace(".sql",""))+".sql";
+        
+                        IF($LocalRevisionDate) {
+                            Clear-Variable LocalRevisionDate
+                        }
+        
+                        IF($LocalModule) {
+                            Clear-Variable LocalModule
+                        }
+        
+                        #Get Modules i.e Inspector and any custom modules and revision dates locally installed
+                        $InstalledVersion = Invoke-sqlcmd -ServerInstance $Servername -Database $LoggingDb -Query $InstalledVersioncmd;
+
+                        #Is Module installed locally
+                        $LocalModule = ($InstalledVersion | ?{$_.Modulename -eq $Modulename}).Modulename
+        
+                        #Get the local Module revision date
+                        $LocalRevisionDate = ($InstalledVersion | ?{$_.Modulename -eq $Modulename}).RevisionDate
+
+
+                        #If the module cannot be found then skip it
+                        IF($LocalModule -eq $null) {
+                            write-host "    No history of $Modulename installed, skipping this module" -ForegroundColor DarkYellow
+                            Continue;
+                        }
+                        
+                        #If the module is found check the revision date and if it is older than the latest or it is null (01/01/1900) then update it
+                        IF((get-date $LocalRevisionDate -Format yyyyMMddHHmmss) -lt (get-date $LastUpdated -Format yyyyMMddHHmmss)) {
+                            write-host "    Updates found for $Modulename, installing update..." -ForegroundColor Cyan
+        
+                            #Get the SQL update script from URL
+                            Try{
+                                write-host "        Retrieving file contents from URL" -ForegroundColor White
+                                Invoke-WebRequest -Uri $URL -Outfile $Scriptfile -ContentType 'sql';         
+                                
+                            } Catch{
+                                write-host "$_.Exception.Message" -ForegroundColor Red;
+                                remove-item -LiteralPath $Scriptfile;
+                                Return;
+                            }
+        
+                            #Execute the SQL retreived from URL
+                            Try{
+                                write-host "        Executing the SQL update script" -ForegroundColor White
+                                Invoke-sqlcmd -ServerInstance $Servername -Database $LoggingDb -InputFile $Scriptfile -ConnectionTimeout 15
+                                remove-item -LiteralPath $Scriptfile;
+        
+                                #If the Inspector build needs updating then the Setup proc needs to be executed following the above revision to the setup proc.
+                                IF($Modulename -eq "InspectorSetup"){
+                                    write-host "        Executing [Inspector].[InspectorSetup] stored procedure" -ForegroundColor White
+                                            
+                                    Invoke-sqlcmd -ServerInstance $Servername -Database $LoggingDb -Query $ExecInspectorSetupProc -ConnectionTimeout 15;
+                                }
+                            } Catch{
+                                write-host "$_.Exception.Message" -ForegroundColor Red;
+                                remove-item -LiteralPath $Scriptfile;
+                                Return;
+                            }
+        
+                        } ELSE {
+                            write-host "    $Modulename up to date" -ForegroundColor Green    
+                        }
+
+        
+                    }
+
+
+            }
+
+        #Update LastUpdated in the AutoUpdate table
+        Invoke-sqlcmd -ServerInstance $CentralServer -Database $LoggingDb -Query "UPDATE [Inspector].[PSAutoUpdate]  SET [LastUpdated] = GETDATE() WHERE [Updatename] = 'PSAutoUpdate';"
+        } ELSE {
+            IF($AutoUpdate -eq 0) {
+                Write-Host "Auto Update: Disabled" -ForegroundColor Yellow;
+            } ELSE {
+                Write-Host "Auto Update: Enabled but Not due yet" -ForegroundColor Yellow;
+                write-host "Update Frequency: Every $($PSAutoUpdateModulesFrequencyMins) mins" -ForegroundColor Yellow;
+                write-host "Last Updated: $PSAutoUpdateLastUpdated" -ForegroundColor Yellow;
+                write-host "Next Update: $(get-date $($PSAutoUpdateLastUpdated.AddMinutes($PSAutoUpdateModulesFrequencyMins)) -Format "dd/MM/yyyy HH:mm:ss")" -ForegroundColor Yellow;
+            }
+        }
+        #endregion
+}
+    Export-ModuleMember -Function InspectorAutoUpdate
