c66af6c3ca7c223ed43c1b29cd488189d6f9a96f
Adrian Buckman <33764798+Adedba@users.noreply.github.com>
2020-05-11 20:35:47 +0100

    Update Install-Inspector.ps1
    #237

--- SQLUndercoverInspector/V2 - Additional files/Install-Inspector.ps1
+++ SQLUndercoverInspector/V2 - Additional files/Install-Inspector.ps1
@@ -20,28 +20,54 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #THIS SCRIPT IS STILL A WORK IN PROGRESS!
 #Author: Adrian Buckman
-#Version: 4.00
-#Revision date: 15/04/2020
+#Version: 5
+#Revision date: 29/04/2020
  
 #set variables
 #if you are using a Github URL this must be the raw URL.
 
-$ScriptPath = "https://raw.githubusercontent.com/SQLUndercover/UndercoverToolbox/master/SQLUndercoverInspector/SQLUndercoverinspectorV2.sql"
+#Set a URL or local path where the SQLUndercoverinspectorV2.sql file exists
+$ScriptPath = "https://raw.githubusercontent.com/SQLUndercover/UndercoverToolbox/Inspector-Dev/SQLUndercoverInspector/SQLUndercoverinspectorV2.sql"
 #$ScriptPath = "C:\Temp\SQLUndercoverinspectorV2.sql"
-$SQLInstances = "SQL01,SQL02,SQL03" #if this is a linked server install ensure that the central server appears first in the list
-$LoggingDB = "SQLUndercover"
+
+#Set a URL or local path of where the Manifest.csv file exists
+$ManifestPath = "https://raw.githubusercontent.com/SQLUndercover/UndercoverToolbox/Inspector-Dev/SQLUndercoverInspector/V2%20-%20Additional%20files/Manifest.csv";
+#$ManifestPath = "C:\Temp\Manifest.csv"
+
+$SQLInstances = "DESTINY" #if this is a linked server install ensure that the central server appears first in the list
+$LoggingDB = "DBA"
+$CustomModules = @("CPU") # "ALL" to install all custommodules or name them "CPU,BlitzWaits,Catalogue,BlitzFileStats"
+
 $UseWindowsAuth = "Y"  #"Y" or "N"
 
 $DataDrive = "S,U"
 $LogDrive = "T,V"
-
 #Optional Parameters
 $BackupsPath = "NULL" #"NULL" or backups Path
 $LinkedServername = "NULL" #"NULL" or Linked server name if you are not using the powershell collection but do need to centralise data into the central server - Linked server must exist.
 $EmailRecipients = "NULL" #Semi colon delimited eg "Email@domain.com;Email2@domain.com"
 
 
+
 #Do not change anything beyond this point
+
+
+$CustomModules = $CustomModules.ToUpper();
+IF(!$CustomModules) {
+    $CustomModules = @("NONE");
+}
+
+#region Validate UseWindowsAuth value
+$UseWindowsAuth = $UseWindowsAuth.ToUpper();
+
+IF ($UseWindowsAuth -notin ("Y","N")) {
+    write-host "Parameter: UseWindowsAuth must have a value of 'Y' or 'N'";
+    Return;
+    }
+
+#endregion
+
+#region check temp dir exists and create if required
 $TempDir = "C:\Temp\"
 
 IF ($TempDir.EndsWith("\") -eq $false) {
@@ -53,22 +79,26 @@ IF ($(test-path -Path $TempDir) -eq $false) {
 }
 
 $TempFilename = $TempDir+"InspectorInstall.sql";
+#endregion
 
-
+#region Windows auth check and set SQL Auth password if required
 IF ($UseWindowsAuth -eq "N") {
 $SQLCred = Get-Credential -Message "Please provide SQL Authentication credentials to connect";
 $SQLUser = $SQLCred.UserName;
 $SQLPassword = $SQLCred.GetNetworkCredential().password
 }
+#endregion
 
 $Confirmation = "";
 
+#region $ScriptPath validation
 IF ($ScriptPath -eq $null) {
     write-host "You must specify a Script Path" -ForegroundColor Red;
     RETURN;
 }
+#endregion
 
-#Determine if the path is a directory or a url
+#region Determine if the path is a directory or a url
 IF ($ScriptPath -like "Http*" -or $ScriptPath -like "www.*") { 
     $ScriptPathType = "URL";
     IF($ScriptPath -notlike "https://raw.githubusercontent.com*") {
@@ -80,9 +110,31 @@ IF ($ScriptPath -like "Http*" -or $ScriptPath -like "www.*") {
 ELSE {
     $ScriptPathType = "Filesystem";
 }
+#endregion
+
+#region $ScriptPath validation
+IF ($ManifestPath -eq $null) {
+    write-host "You must specify a Manifest Path" -ForegroundColor Red;
+    RETURN;
+}
+#endregion
+
+#region Determine if the Manifest path is a directory or a url
+IF ($ManifestPath -like "Http*" -or $ManifestPath -like "www.*") { 
+    $ManifestPathType = "URL";
+    IF($ManifestPath -notlike "https://raw.githubusercontent.com*") {
+    write-host "Github URL must be a raw type URL link e.g https://raw.githubusercontent.com..." -ForegroundColor Red;
+    Return;
+    }
+
+} 
+ELSE {
+    $ManifestPathType = "Filesystem";
+}
+#endregion
 
 
-#Retrieve the Inspector Installation SQL from URL
+#region Retrieve the Inspector Installation SQL from URL
 IF ($ScriptPathType -eq "URL") {
     Try {
     Invoke-WebRequest $ScriptPath -Outfile $TempFilename -ContentType 'sql';
@@ -99,9 +151,9 @@ IF ($ScriptPathType -eq "URL") {
         write-host "Script path - Ok" -ForegroundColor Green;
     }
 }
+#endregion
 
-
-#Validate script contents
+#region Validate script contents
 write-host "Validating script contents";
 
 #$InspectorURL = (Get-content -Path $(IF ($ScriptPathType -eq "URL") {$TempFilename} ELSE {$ScriptPath}) -TotalCount 74)[-1]
@@ -120,7 +172,9 @@ IF ($([decimal]::TryParse($Build,[ref]0.00)) -eq $false) {
     Write-host "Inspector build failed validation" -ForegroundColor Red
     RETURN;
 }
+#endregion
 
+#region confirm before continuing
 While ($Confirmation -notin ("Y","N")) {
     $Confirmation = Read-host -Prompt "You are about to install Undercover Inspector Build: $Build in Database: $LoggingDB on servers: $SQLInstances
 are you happy to continue Y/N?";
@@ -137,7 +191,9 @@ are you happy to continue Y/N?";
 }
 
 $Confirmation = "";
+#endregion
 
+#region sanitise additional params
 IF ($BackupsPath -ne "NULL") {
 $BackupsPath = "'"+$BackupsPath+"'";
 };
@@ -149,8 +205,9 @@ $LinkedServername = "'"+$LinkedServername+"'";
 IF ($EmailRecipients -ne "NULL") {
 $EmailRecipients = "'"+$EmailRecipients+"'";
 };
+#endregion
 
-
+#region Set SQL statement text
 $InstallScript = "
 EXEC [Inspector].[InspectorSetup]
 --Required Parameters (No defaults)							     
@@ -178,9 +235,10 @@ EXEC [Inspector].[InspectorSetup]
 ";
 
 $DBExistsQry = "SELECT CASE WHEN DB_ID('$LoggingDB') IS NOT NULL THEN 1 ELSE 0 END AS DB_ID;";
+#endregion
 
 
-#For Each server create the InspectorSetup stored proc and execute it to install
+#region For Each server create the InspectorSetup stored proc and execute it to install
 ForEach ($SQLInstance in $SQLInstances.Split(","))
 {
     switch ($UseWindowsAuth) {
@@ -240,11 +298,85 @@ ForEach ($SQLInstance in $SQLInstances.Split(","))
     }
     
 }
+#endregion
 
+#region start custom module install 
 
-#Remove the Installation file
+IF ($ManifestPathType -eq "URL") {
+    invoke-webrequest -Uri $ManifestPath -Outfile $TempFilename -ContentType 'csv';
+    $ManifestContents = import-csv -Path $TempFilename
+
+    #Grab the contents of the manifest excluding the Inpector Install file
+    foreach($ManifestItem in $($ManifestContents | ?{$_.Modulename -ne "SQLUndercoverinspectorV2.sql"})) {
+        $ModuleFilename = $ManifestItem.Modulename;
+        $ModuleURL = $ManifestItem.URL;
+        $Modulename = $($ManifestItem.Modulename).Replace("Inspector_","").Replace("_CustomModule.sql","");
+
+        IF((($CustomModules -eq "ALL") -or $($CustomModules -contains $Modulename)) -eq $true) {
+
+            write-host "Installing $ModuleFilename";
+            invoke-webrequest -Uri $ModuleURL -Outfile $TempFilename -ContentType 'sql';
+
+            IF ($(test-path -Path $TempFilename) -eq $true) {
+
+                switch ($UseWindowsAuth) {
+                "Y" { 
+                        Invoke-Sqlcmd -InputFile $TempFilename -ServerInstance $SQLInstance -database $LoggingDB -ConnectionTimeout 10;
+                     }
+                    
+                "N" {  
+                        Invoke-Sqlcmd -InputFile $TempFilename -ServerInstance $SQLInstance -database $LoggingDB -Username $SQLUser -Password $SQLPassword -ConnectionTimeout 10;
+                    }
+                }
+            } ELSE {
+                write-host "File: $TempFilename does not exist - unable to install module: $ModuleFilename" -ForegroundColor Red;
+            }
+        }
+    }
+} ELSE {
+    IF ($(test-path -Path $ManifestPath) -eq $true) {
+        $ManifestContents = import-csv -Path $ManifestPath;
+
+        #Grab the contents of the manifest excluding the Inpector Install file
+        foreach($ManifestItem in $($ManifestContents | ?{$_.Modulename -ne "SQLUndercoverinspectorV2.sql"})) {
+            $ModuleFilename = $ManifestItem.Modulename;
+            $ModulePath = $(split-path -Path $ManifestPath)+"\"+$ModuleFilename;
+            $Modulename = $($ManifestItem.Modulename).Replace("Inspector_","").Replace("_CustomModule.sql","");
+
+        IF((($CustomModules -eq "ALL") -or $($CustomModules -contains $Modulename)) -eq $true) {
+
+            write-host "Installing $ModuleFilename";
+            
+            IF ($(test-path -Path $ModulePath) -eq $true) {
+
+                switch ($UseWindowsAuth) {
+                "Y" { 
+                        Invoke-Sqlcmd -InputFile $ModulePath -ServerInstance $SQLInstance -database $LoggingDB -ConnectionTimeout 10;
+                     }
+                    
+                "N" {  
+                        Invoke-Sqlcmd -InputFile $ModulePath -ServerInstance $SQLInstance -database $LoggingDB -Username $SQLUser -Password $SQLPassword -ConnectionTimeout 10;
+                    }
+                }
+            } ELSE {
+                write-host "File: $ModulePath does not exist - unable to install module: $ModuleFilename" -ForegroundColor Red;
+            }
+
+            }
+
+        }
+    }
+
+}
+
+#endregion
+
+#region Remove the Installation file
 IF ($ScriptPathType -eq "URL") {
     Remove-Item -Path $TempFilename;
 }
+#endregion 
 
+#region clear variables
 Clear-Variable ScriptPathType,ScriptPath,SQLInstances,LoggingDB,TempDir,DataDrive,LogDrive,BackupsPath,LinkedServername,UseWindowsAuth,InspectorURL,Build;
+#endregion
