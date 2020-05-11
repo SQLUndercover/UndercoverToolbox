0b912059035f8a99f86753ba11cbde58d6ff8eb9
SQLUndercover <david.fowler1@outlook.com>
2020-05-11 13:00:44 +0100

    Create Catalogue_UD043.sql

---
+++ SQLUndercoverCatalogue/Updates/Catalogue_UD043.sql
@@ -0,0 +1,137 @@
+/*                                                                                                                      
+                                                                                                                                                                                                                                             
+              @     ,@                                                                                                  
+             #@@   @@@                                                                                                  
+             @@@@@@@@@;                                                                                                 
+             @@@@@@@@@@                                                                                                 
+            :@@@@@@@@@@                                                                                                 
+            @@@@@@@@@@@                                                                                                 
+            @@@@@@@@@@@;                                                                                                
+            @@@@@@@@@@@@                                                                                                
+            @@@@@@@@@@@@                                                                                                
+           `+@@@@@@@@@@+                                                                                                
+                                                                                                                        
+                                                                                                                        
+         .@@`           #@,                                                                                             
+     .@@@@@@@@@@@@@@@@@@@@@@@@:                                                                                         
+   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        @@     @@   @@      #@   @           @                                         
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@      @@@@   @@@@  @#      #@   @           @                                         
+    ;@@@@@@@@@@@@@@@@@@@@@@@@@@'        @     @   @# @#      #@   @ #@@@   @@@@  @@@  @@@@@  @@@   @@  @   @  @@   @ @@     
+        .+@@@@@@@@@@@@@@@@+.            @@@@  @   @@ @#      #@   @ #@  @ @@  @  @  @  @@   @  @  @ `@  @ @  @  @  @@    
+       '`                  `,#           @@@@ @   @@ @#      #@   @ #@  @ @#  @ @@@@@  @    @     @  @  @ @  @@@@  @`     
+     ,@@@@ '@@@@@@@@@@@@@ .@@@@;           @  @   @@ @#      #@   @ #@  @ @@  @ @@     @ `  @     @  @  @@@  @     @      
+    #@@@@@@ @@@@@  +@@@@  +@@@@@@       @@@@   @@@@  @@@@@   `@@@@@ #@  @ #@ @@  @  @  @    @@ @  @  @   @   @  @  @      
+   @@@@@@@@  ,#.    `#;   @@@@@@@@'      @@     @@   @@@@@     @@,  #@  @  @@ @   @@  @@     #@    @@    @    @@   @      
+  ;#@@@@@@@@             @@@@@@@@@#,              @                                                                     
+       ,@@@@+           @@@@@+`                                                                                         
+          .@@`        `@@@@                                          www.sqlundercover.com                                                             
+         +@@@@        @@@@@+                                                                                            
+        @@@@@@@      @@@@@@@@#                                                                                          
+         @@@@@@@    @@@@@@,                                                                                             
+         @@@@@@@    @@@@@@,                                                                                             
+           :@@@@@' ;@@@@`                                                                                               
+             `@@@@ @@@+                                                                                                 
+                @#:@@                                                                                                   
+                  @@                                                                                                    
+                  @`                                                                                                    
+                  #                                                                                                     
+                                                                                                                            
+Sequential Upgrade - 0.4.3
+David Fowler
+27/04/2020
+
+MIT License
+------------
+
+Copyright 2020 Sql Undercover
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+
+*/
+
+--local interrogation proc
+--add update to execution logs
+
+ALTER PROC [Catalogue].[LocalInterrogation]
+AS
+
+BEGIN
+
+SET NOCOUNT ON
+
+DECLARE @GetDefinition NVARCHAR(MAX)
+DECLARE @UpdateDefinition NVARCHAR(MAX)
+DECLARE @StageTableName NVARCHAR(128)
+DECLARE @cmd NVARCHAR(MAX)
+
+--Update execution audit
+INSERT INTO Catalogue.ExecutionLog(ExecutionDate) VALUES(GETDATE())
+
+DECLARE Modules CURSOR STATIC FORWARD_ONLY
+FOR
+	SELECT GetDefinition, UpdateDefinition, StageTableName
+	FROM Catalogue.ConfigModules
+	JOIN Catalogue.ConfigModulesDefinitions 
+		ON ConfigModules.ID = ConfigModulesDefinitions.ModuleID
+	LEFT OUTER JOIN Catalogue.ConfigModulesInstances
+		ON Catalogue.ConfigModules.ModuleName = ConfigModulesInstances.ModuleName 
+		AND ConfigModulesInstances.ServerName = @@SERVERNAME
+	WHERE ISNULL(ConfigModulesInstances.Active, ConfigModules.Active) = 1
+	--AND ModuleName = 'Databases'
+
+OPEN Modules
+
+FETCH NEXT FROM Modules INTO @GetDefinition, @UpdateDefinition, @StageTableName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	--truncate stage tables
+	EXEC ('TRUNCATE TABLE Catalogue.' + @StageTableName )
+
+	--insert into stage tables
+	SET @cmd = N'INSERT INTO Catalogue.' + @StageTableName + ' EXEC (@GetDefinition)'
+
+	EXEC sp_executesql @cmd, N'@GetDefinition VARCHAR(MAX)', @GetDefinition = @GetDefinition
+	
+	--execute update code
+	EXEC sp_executesql @UpdateDefinition
+
+	FETCH NEXT FROM Modules INTO @GetDefinition, @UpdateDefinition, @StageTableName
+
+END
+
+CLOSE Modules
+DEALLOCATE Modules
+
+--Mark execution complete
+UPDATE Catalogue.ExecutionLog SET CompletedSuccessfully = 1 FROM Catalogue.ExecutionLog WHERE ID = (SELECT MAX(ID) FROM Catalogue.ExecutionLog)
+
+END
+GO
+
+----------------------------------------------------------------------------
+--Schema Changes
+----------------------------------------------------------------------------
+
+ALTER TABLE Catalogue.Tables 
+ADD Rows BIGINT NULL,
+	TotalSizeMB BIGINT NULL,
+	UsedSizeMB BIGINT NULL
+GO
+
+ALTER TABLE Catalogue.Tables_Stage
+ADD Rows BIGINT NULL,
+	TotalSizeMB BIGINT NULL,
+	UsedSizeMB BIGINT NULL
+GO
+
