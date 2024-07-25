4b96645aac121b97649e74d3fe0b6c5deb0811d9
David Fowler <42606354+UndercoverDavid@users.noreply.github.com>
2024-07-25 15:31:41 +0100

    Catalogue 0.5.0 upgrade script
    Database schema upgrade script for upcoming 0.5.0 release

---
+++ SQLUndercoverCatalogue/Updates/Catalogue_UD050.sql
@@ -0,0 +1,85 @@
+/*                                                                                                                      
+                                                                                                                                                                                                                                             
+              @     ,@                                                                                                  
+             #@@   @@@                                                                                                  
+             @@@@@@@@@;                                                                                                 
+             @@@@@@@@@@                                                                                                 
+            :@@@@@@@@@@                                                                                                 
+            @@@@@@@@@@@                                                                                                 
+            @@@@@@@@@@@;                                                                                                
+            @@@@@@@@@@@@                                                                                                
+            @@@@@@@@@@@@                                                                                                
+           `+@@@@@@@@@@+                                                                                                
+                                                                                                                        
+                                                                                                                        
+         .@@`           #@,                                                                                             
+     .@@@@@@@@@@@@@@@@@@@@@@@@:                                                                                         
+   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        @@     @@   @@      #@   @           @                                         
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@      @@@@   @@@@  @#      #@   @           @                                         
+    ;@@@@@@@@@@@@@@@@@@@@@@@@@@'        @     @   @# @#      #@   @ #@@@   @@@@  @@@  @@@@@  @@@   @@  @   @  @@   @ @@     
+        .+@@@@@@@@@@@@@@@@+.            @@@@  @   @@ @#      #@   @ #@  @ @@  @  @  @  @@   @  @  @ `@  @ @  @  @  @@    
+       '`                  `,#           @@@@ @   @@ @#      #@   @ #@  @ @#  @ @@@@@  @    @     @  @  @ @  @@@@  @`     
+     ,@@@@ '@@@@@@@@@@@@@ .@@@@;           @  @   @@ @#      #@   @ #@  @ @@  @ @@     @ `  @     @  @  @@@  @     @      
+    #@@@@@@ @@@@@  +@@@@  +@@@@@@       @@@@   @@@@  @@@@@   `@@@@@ #@  @ #@ @@  @  @  @    @@ @  @  @   @   @  @  @      
+   @@@@@@@@  ,#.    `#;   @@@@@@@@'      @@     @@   @@@@@     @@,  #@  @  @@ @   @@  @@     #@    @@    @    @@   @      
+  ;#@@@@@@@@             @@@@@@@@@#,              @                                                                     
+       ,@@@@+           @@@@@+`                                                                                         
+          .@@`        `@@@@                                          www.sqlundercover.com                                                             
+         +@@@@        @@@@@+                                                                                            
+        @@@@@@@      @@@@@@@@#                                                                                          
+         @@@@@@@    @@@@@@,                                                                                             
+         @@@@@@@    @@@@@@,                                                                                             
+           :@@@@@' ;@@@@`                                                                                               
+             `@@@@ @@@+                                                                                                 
+                @#:@@                                                                                                   
+                  @@                                                                                                    
+                  @`                                                                                                    
+                  #                                                                                                     
+                                                                                                                            
+Sequential Upgrade - 0.5.0
+David Fowler
+25/07/2024
+
+MIT License
+------------
+
+Copyright 2024 Sql Undercover
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+
+Change Log
+----------
+
+NEW TABLE - EventLog, used to store a detailed runtime event log - requires the SQLUndercover Interrogator service
+
+*/
+
+---------------------------------------------------
+--Schema Changes
+---------------------------------------------------
+
+CREATE TABLE Catalogue.EventLog
+(ID INT IDENTITY(1,1),
+LogDate Datetime,
+Message VARCHAR(200))
+
+--update versions tables
+
+UPDATE Catalogue.ConfigPoSH
+SET ParameterValue = '0.5.0'
+WHERE ParameterName = 'CatalogueVersion'
+
+--update history
+
+INSERT INTO Catalogue.UpgradeHistory (UpgradeVersion, UpgradeDate)
+VALUES ('0.5.0', GETDATE())
